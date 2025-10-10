0300479c467a569587cb604186b2682779c66679
gutofm <gutofisco@gmail.com>
2025-10-10 05:00:32 -0300

    Index Navigation changes

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,33 +1,28 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from '../screens/Login';
 import Home from '../screens/Home';
-// import CreateUser from '../screens/admin/CreateUser';
-import { useAuth } from '../contexts/AuthContext';
+import {useAuth} from '../contexts/AuthContext';
 
 export type RootStackParamList = {
   Login: undefined;
   Home: undefined;
-  CreateUser: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Routes() {
-  const { user, loading } = useAuth();
-  if (loading) return null; // pode colocar um Splash
+  const {user, loading} = useAuth();
+  if (loading) return null;
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         {user ? (
-          <>
-            <Stack.Screen name="Home" component={Home} />
-            {/*        <Stack.Screen name="CreateUser" component={CreateUser} />              */}
-          </>
+          <Stack.Screen name="Home" component={Home}/>
         ) : (
-          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Login" component={Login}/>
         )}
       </Stack.Navigator>
     </NavigationContainer>
